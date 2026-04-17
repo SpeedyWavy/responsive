d1445fdf2f8ef2c4f9507fdd8fad04de9a9066dd
SpeedyWavy <46286725+SpeedyWavy@users.noreply.github.com>
2026-04-17 16:11:47 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -10,17 +10,23 @@ function App() {
      <div className="imagem"></div>
      <div className="login">
       <div className="logo"></div>
-      <h1>Realizar Login</h1>
+      <h1>Iniciar Sessão</h1>
       <div className="ui-input">
-        <p>Login</p>
-        <input type="text" placeholder='Digite seu Login'/>
+        <p id='sessao'>Iniciar Sessão com Nome de Usuario</p>
+        <input type="text" placeholder='Digite seu Usuario'/>
         <p>Senha</p>
         <input type="text" placeholder='Digite sua Senha'/>
-      </div>
+
+        <div className="lembrar">
+        <input type="checkbox" id='checkbox' />
+        <p id='lembre'>Lembre-Me</p>
+        </div>
+      
 
       <div className="opcoes">
-        <button>Entrar</button>
-        <a href="" className='esqueci'>Esqueci meu Login ou Senha</a>
+        <button>Iniciar Sessão</button>
+        <a href="" className='esqueci'>Não Consigo Iniciar a Sessão</a>
+      </div>
       </div>
      </div>
      </main>
